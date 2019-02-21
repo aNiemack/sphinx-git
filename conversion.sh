55dece6d8ff1f92bdcd64cee20f74d77b86b682f
Andrew Niemack <45608900+aNiemack@users.noreply.github.com>
2019-02-21 13:09:43 -0700

    Update conversion.sh

--- conversion.sh
+++ conversion.sh
@@ -13,28 +13,25 @@ git config --global credential.helper cache
 git config --global user.email "$EMAIL"
 git config --global user.name "$USER"
 
-git svn clone https://svn-dares-dart.cgd.ucar.edu/DART --ignore-paths="^trunk" --no-metadata --revision "$REVISION:HEAD"
+BRANCHES="$(cat branches.txt)"
+git svn clone https://svn-dares-dart.cgd.ucar.edu/DART --include-paths="^(?:$BRANCHES) --ignore-paths="FEOM/mizzi" --no-metadata --revision "$REVISION:HEAD"
 cd DART
 
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch branches/nancy_new/models/bgrid_solo/work/obs_seq.in' --prune-empty --tag-name-filter cat -- --all
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch branches/location_utils/models/cice/work/cice.r.nc' --prune-empty --tag-name-filter cat -- --all
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch branches/radiance/observations/obs_converters/AIRS/readers/V6_FORTRAN_C_READERS.tar.gz' --prune-empty --tag-name-filter cat -- --all
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch branches/rma_pop_static_data/models/POP/work/inputdata/ts_PHC2_jan_ic_gx1v6_20090205.ieeer8' --prune-empty --tag-name-filter cat -- --all
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch branches/recam/models/cam-fv/work/caminput.nc' --prune-empty --tag-name-filter cat -- --all
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch branches/rma_pop_static_data/models/POP/work/pop.r.nc' --prune-empty --tag-name-filter cat -- --all
-
-git for-each-ref --format='delete %(refname)' refs/original | git update-ref --stdin
-git reflog expire --expire=now --all
-git gc --prune=now
-
-for element in "$@"; do
+while IFS='|' read -r line < branches.txt || [[ -n "$line" ]]; do
         
-        IFS='/' read -ra array <<< "$element"
-        cd $element
+        IFS='/' read -ra array <<< "$line"
+        cd $line
         git init
         git add --all
         git commit -m "initilize git"
         git checkout -b "${array[-1]}"
+        ls -lRt | grep -E " [1-9].{8,}" > large.txt
+        while IFS=' ' read -ra large < large.txt || [[ -n "$large" ]]; do
+                path = "$(find -name ${large[6]}
+                git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch $path' --prune-empty --tag-name-filter cat -- --all
+        git for-each-ref --format='delete %(refname)' refs/original | git update-ref --stdin
+        git reflog expire --expire=now --all
+        git gc --prune=now      
         git add --all
         git commit -m "from svn to github"
         git push --set-upstream  $REMOTE "${array[-1]}"
